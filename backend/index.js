d757317ce2dfef924efcabfd66b10a4820b10246
sachinchavda17 <sachinchavda321@gmail.com>
2024-04-28 16:46:45 +0530

    connectToMongodb removed

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import path from "path"
-import authRoutes from  './routes/auth.js';
-import messageRoutes from  './routes/messages.js';
+import authRoutes from './routes/auth.js';
+import messageRoutes from './routes/messages.js';
 import userRoutes from "./routes/user.js"
 import connectToMongoDb from "./db/ConnectToMongoDb.js";
 import { app, server } from "./socket/socket.js";
+import mongoose from "mongoose";
 
 const newDirname = path.resolve();
 
@@ -17,19 +18,27 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cookieParser())
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(newDirname, "/frontend/dist")))
 
-app.get("*",(req,res)=>path.join(newDirname,"frontend","dist","index.html") )
+app.get("*", (req, res) => path.join(newDirname, "frontend", "dist", "index.html"))
 
 // app.listen(port, () => {
 //     connectToMongoDb();
 //     console.log(`Server is running on port ${port}`)
 // })
-server.listen(port, () => {
-    connectToMongoDb();
+
+try {
+     mongoose.connect(process.env.MONGODB_URI)
+    console.log("Conected to MongoDB")
+} catch (error) {
+    console.log("Error Connecting to mongoDB " + error.message)
+}
+
+server.listen(port,  () => {
+    // connectToMongoDb();
     console.log(`Server is running on port ${port}`)
 })
