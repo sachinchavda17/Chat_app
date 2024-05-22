7a9de63ea5321b430c0d5eaafab301335972d9e0
sachinchavda17 <sachinchavda321@gmail.com>
2024-05-22 15:20:04 +0530

    set as before

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: { port: 3000 ,
-//   proxy:{
-//     "/api":{
-//       target:"http://localhost:5000"
-//     }
-//   }
-//   },
-// })
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false
-      }
+  server: { port: 3000 ,
+  proxy:{
+    "/api":{
+      target:"http://localhost:5000"
     }
-  },
-  build: {
-    outDir: 'dist'  // Ensure the build output directory is 'dist'
   }
+  },
 })
